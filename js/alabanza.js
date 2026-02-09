0eb157a7a88122a2851384b28c36f30a00444f01
martinezdiegoe-ux <martinezdiegoe@gmail.com>
2026-02-09 10:44:20 -0300

    Update alabanza.js

--- js/alabanza.js
+++ js/alabanza.js
@@ -1,14 +1,37 @@
 const datosAlabanzas = {
   alabanza1: {
-    titulo: "Cuán grande es Dios",
+    titulo: "Te lo debo a Ti",
     letra: `
-Cuán grande es Dios,
-cántale cuán grande es Dios,
-y todos lo verán,
-cuán grande es Dios.
+I
+Si vivo es porque Tú Vives,
+Soy Salvo pues Tú me Salvaste,
+Si canto Tú me diste alegría,
+Si ando Tú me Levantaste.
+Y es que tengo mil razones
+Que declaran Tu Grandeza,
+Lo que soy y lo que tengo
+Te lo Debo a Ti.
+Coro:
+Te lo Debo a Ti que viniste a morir
+Para darme alegría,
+Tú cargabas la cruz, Tú sufrías por mí,
+Tú pensabas en mí.
+Tú viniste a cambiar mi tristeza en canción
+Y mis noches en día,
+Si hoy tengo un lugar, si me espera un hogar
+Te lo Debo a Ti.
+II
+No tengo recuerdos amargos
+Porque Todo Señor por amor lo has llevado;
+Y aquello que fue en mi vida es historia,
+De Tu Fidelidad solo tengo memoria.
+Y es que tengo mil razones
+Que declaran Tu Grandeza
+Lo que soy y lo que tengo
+Te lo Debo a Ti.
     `,
-    pdf: "pdf/cuan-grande-es-dios.pdf",
-    youtube: "https://www.youtube.com/watch?v=xxxxx"
+    pdf: "pdf/Te-lo-debo-a-Ti.pdf",
+    youtube: "https://youtu.be/58pJyGiIjeM?si=opU4nilvjZTovfSO"
   },
 
   alabanza2: {
