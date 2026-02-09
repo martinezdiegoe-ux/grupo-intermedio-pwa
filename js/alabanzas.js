c7a3f52ee3e79b46e1fb3cc16ca204e3ae88c98d
martinezdiegoe-ux <martinezdiegoe@gmail.com>
2026-02-09 09:48:08 -0300

    Create alabanzas.js

---
+++ js/alabanzas.js
@@ -0,0 +1,29 @@
+const alabanzas = [
+  {
+    id: "alabanza1",
+    titulo: "Cuán grande es Dios"
+  },
+  {
+    id: "alabanza2",
+    titulo: "Digno es el Cordero"
+  },
+  {
+    id: "alabanza3",
+    titulo: "Santo por siempre"
+  }
+];
+
+// Render listado
+const lista = document.getElementById("lista-alabanzas");
+
+alabanzas.forEach(alabanza => {
+  const div = document.createElement("div");
+  div.className = "alabanza-item";
+  div.textContent = alabanza.titulo;
+
+  div.onclick = () => {
+    window.location.href = `alabanza.html?id=${alabanza.id}`;
+  };
+
+  lista.appendChild(div);
+});
