bd7eea9cd960941e41e3d10c6d3a46e3be78282f
martinezdiegoe-ux <martinezdiegoe@gmail.com>
2026-02-10 07:12:00 -0300

    Remove enviarMensaje function
    Removed the enviarMensaje function that sends an email.

--- js/novedades.js
+++ js/novedades.js
@@ -2,11 +2,4 @@ function irA(pagina) {
   window.location.href = pagina;
 }
 
-function enviarMensaje() {
-  const correo = "martinezdiegoe@gmail.com"; // 👈 CAMBIÁ ESTE MAIL
-  const asunto = "Mensaje desde la app - Grupo Intermedio";
-  const cuerpo = "Hola, quisiera hacer una consulta:";
-
-  const mailto = `mailto:${correo}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(cuerpo)}`;
-  window.location.href = mailto;
 }
